997bdbb4db8979169bef4a61357430dfe7d34f98
PoliakovSergei <poliakov.s.e@yandex.ru>
2021-07-04 21:38:06 +0300

    Added expenses-calculation service / restructure of project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
-rootProject.name = "todo-web-app"
+rootProject.name = "daily-helper-app"
 include(
-    "todo-back",
-    "todo-front",
-    "todo-docker"
+    "todo-web-app:todo-back",
+    "todo-web-app:todo-front",
+    "todo-web-app:todo-docker",
+    "expenses-calculation-service",
+    "expenses-calculation-service:ecs-back",
+    "expenses-calculation-service:ecs-front",
+    "expenses-calculation-service:ecs-docker"
 )
