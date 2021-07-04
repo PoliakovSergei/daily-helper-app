b5b254fb0c6609e54e64a1e58c8215ee51df6c97
PoliakovSergei <poliakov.s.e@yandex.ru>
2021-07-04 20:25:33 +0300

    Small build.gradle.kts changes

--- todo-back/build.gradle.kts
+++ todo-back/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     id("org.springframework.boot") version "2.0.1.RELEASE"
-    id("io.freefair.lombok") version "6.0.0-m2"
 }
 
 val mainClassPath = "ru.sergo.todoapp.todoback.TodoBackApplication"
@@ -24,13 +23,14 @@ springBoot {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:2.3.4.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-web:2.3.4.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.3.4.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-security:2.3.4.RELEASE")
     implementation("org.liquibase:liquibase-core:3.8.9")
     implementation("org.postgresql:postgresql:42.2.16")
     implementation("org.projectlombok:lombok:1.18.12")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.12")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.3.4.RELEASE")
 }
